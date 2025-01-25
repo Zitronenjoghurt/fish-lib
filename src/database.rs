fbe31730178a12c1caf402be2c43148f07845af0
Zitronenjoghurt <gersdorfdominik@gmail.com>
2025-01-25 19:56:38 +0100

    change database reset method once again

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,5 @@
 use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
-use diesel::sql_types::Text;
-use diesel::{PgConnection, QueryableByName, RunQueryDsl};
+use diesel::{PgConnection, QueryableByName};
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
@@ -50,32 +49,13 @@ impl Database {
     pub fn clear(&self) -> Result<(), Box<dyn std::error::Error>> {
         let mut connection = self.get_connection()?;
 
-        println!("Attempting to clear database...");
+        connection
+            .revert_all_migrations(MIGRATIONS)
+            .map_err(|e| format!("Failed to rollback migrations: {}", e))?;
 
-        #[derive(QueryableByName, Debug)]
-        struct TableName {
-            #[diesel(sql_type = Text)]
-            tablename: String,
-        }
-
-        let table_names: Vec<String> =
-            diesel::sql_query("SELECT tablename FROM pg_tables WHERE schemaname = 'public'")
-                .load::<TableName>(&mut connection)?
-                .into_iter()
-                .map(|table| table.tablename)
-                .collect();
-
-        println!("Tables to truncate: {:?}", table_names);
-
-        for table in table_names {
-            println!("Truncating table: {}", table);
-            match diesel::sql_query(format!("TRUNCATE TABLE {} RESTART IDENTITY CASCADE", table))
-                .execute(&mut connection)
-            {
-                Ok(_) => println!("Successfully truncated {}", table),
-                Err(e) => println!("Error truncating {}: {:?}", table, e),
-            }
-        }
+        connection
+            .run_pending_migrations(MIGRATIONS)
+            .map_err(|e| format!("Failed to re-run migrations: {}", e))?;
 
         Ok(())
     }
