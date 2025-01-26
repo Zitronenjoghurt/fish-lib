0cde53f4fca90579c797d77973d1b8164be8883e
Zitronenjoghurt <gersdorfdominik@gmail.com>
2025-01-26 17:12:49 +0100

    update user and fish service usage in example project

--- example/src/tests/test_database.rs
+++ example/src/tests/test_database.rs
@@ -1,13 +1,13 @@
 use crate::tests::setup_test;
-use fish_lib::game::repositories::fish_repository::FishRepository;
-use fish_lib::game::repositories::user_repository::UserRepository;
+use fish_lib::game::services::fish_service::FishService;
+use fish_lib::game::services::user_service::UserService;
 
 #[test]
 fn test_database() {
     setup_test();
 
-    let user = UserRepository::create_from(1337).unwrap();
-    let fish = FishRepository::create_from(&user, 1).unwrap();
+    let user = UserService::create_and_save_user(1337).unwrap();
+    let fish = FishService::generate_and_save_fish(&user, 1).unwrap();
 
     assert_eq!(user.external_id, 1337);
     assert_eq!(fish.user_id, user.id);
